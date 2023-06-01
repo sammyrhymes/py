13867dac5c9b8b67e41ab9843d0aaceb603c82b3
sammyrhymes <samuelmwendwa5996@gmail.com>
2023-06-01 16:10:20 +0300

    on a roll now baby

--- network_programming/simpleBrowsere3.py
+++ network_programming/simpleBrowsere3.py
@@ -1,9 +1,36 @@
-# import the neccesary urllib modules(request, parse and error)
-import urllib.request , urllib.parse , urllib.error
+import urllib.request
+import re
 
-# use urllib.request and urlopen() to open the link and get the encoded data 
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+# URL 1
+url1 = 'http://www.dr-chuck.com/page1.htm'
+while True:
+    url2 = ''
 
-# decode the data and print it : remember to shred the empty lines using strip() function 
-for line in fhand:
-    print(line.decode().strip())
+    # Open the URL and retrieve the HTML content
+    fhand = urllib.request.urlopen(url1)
+
+    # Decode the data and print it line by line
+    for line in fhand:
+        text = line.decode().strip()
+
+        # Check for links using regex
+        pattern = r'<a\s+href=(?:"([^"]+)"|\'([^\']+)\'|([^ >]+)).*>'
+        match = re.search(pattern, text)
+        if match:
+            link = match.group(1) or match.group(2) or match.group(3)
+            print(link)
+
+            if link == '':
+                break
+
+            # Copy the link to URL 2
+            url2 = link
+            break
+
+    # Compare URL 1 and URL 2
+    if url1 == url2:
+        break
+
+    url1 = url2  # Assign URL 2 to URL 1 for the next iteration
+
+print("No more links found. Exiting...")
